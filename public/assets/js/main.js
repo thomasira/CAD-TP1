7e53100c6f282cb288f73b541fdc266a939483a2
thomasIRA <thomas.al@live.ca>
2023-12-09 20:31:39 -0500

    play with more text

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,7 @@
 import anime from "./animejs/anime.js";
 import Typed from "./typed/typed.js";
 
-var typed = new Typed('[data-js-type]', {
+new Typed('[data-js-type="1"]', {
     strings: ["Forum"],
     cursorChar: '_',
     typeSpeed: 200,
@@ -16,19 +16,27 @@ var typed = new Typed('[data-js-type]', {
 
 document.addEventListener('endType', () => {
     const elCursor = document.querySelector('.typed-cursor');
-    const elType = document.querySelector('[data-js-type]');
-    elType.innerHTML = elType.textContent.replace(/\S/g,'<span class="letter">$&</span>');
+    const elType1 = document.querySelector('[data-js-type="1"]');
+    const elType2 = document.querySelector('[data-js-type="2"]');
+    const elMain = document.querySelector('[data-js-main]');
+    elType1.innerHTML = elType1.textContent.replace(/\S/g,'<span class="letter">$&</span>');
 
-    const elLetters = Array.from(elType.querySelectorAll('.letter'));
+    const elLetters = Array.from(elType1.querySelectorAll('.letter'));
     elLetters.push(elCursor);
 
-    anime({
+    const typeFall = anime({
         targets: elLetters,
-        translateY: 1000,
+        translateY: 800,
         delay: function() { return anime.random(0, 1000); },
         duration: function() { return anime.random(4000, 8000); },
         rotate: function() { return anime.random(-220, 220); },
         easing: 'easeOutExpo',
-        complete: function() { elType.remove(); elCursor.remove(); }
+        complete: function() { elType1.remove(); elCursor.remove(); }
     });
+    new Typed('[data-js-type="2"]', {
+        strings: ["Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non expedita est, reiciendis delectus voluptates incidunt voluptate rerum omnis dignissimos illo ea. Tenetur aliquid reprehenderit, qui delectus recusandae mollitia quo cumque! "],
+        showCursor: false,
+        typeSpeed: 40,
+    });
+    elMain.classList.remove('hidden');
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,24 @@
 @section('title', 'Accueil')
 @section('content')
 <div class="home" data-js-forum>
-
-    <div data-js-type>
+    <div data-js-type="1">
         <!-- string is defined and animated in JS -->
     </div>
-    
+
+    <main data-js-main class="hidden">
+        <section>
+            <header>
+                <div data-js-type="2">
+                    <!-- string is defined and animated in JS -->
+                </div>
+            </header>
+            <div>
+                <a href="" class="btn">Entrez</a>
+                <a href="" class="btn"></a>
+            </div>
+
+        </section>
+    </main>
+
 </div>
 @endsection
